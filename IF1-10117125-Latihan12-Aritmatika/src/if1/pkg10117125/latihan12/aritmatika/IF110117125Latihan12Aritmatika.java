fa6e776e5eee2f7b4486494a0eaa462657fe43a9
zulfiihzam <35595962+zulfiihzam@users.noreply.github.com>
2020-10-21 00:54:08 +0700

    Add files via upload

---
+++ IF1-10117125-Latihan12-Aritmatika/src/if1/pkg10117125/latihan12/aritmatika/IF110117125Latihan12Aritmatika.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10117125.latihan12.aritmatika;
+
+/**
+ *
+ * @author 
+ * Nama  : Zulfi Ihzam Rahmat
+ * Kelas : IF1
+ * NIM   : 10117125
+ * Deskrpisi Program : berisi program menghitung 
+ * menggunakan rumus aritmatika
+ */
+public class IF110117125Latihan12Aritmatika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("b / a = " + (b / a));
+        System.out.println("b % a = " + (b % a));
+        System.out.println("c % a = " + (c % a));
+        System.out.println("a++ = " + (a++));
+        System.out.println("b-- = " + (b--));
+        System.out.println("d++ = " + (d++));
+        System.out.println("++d = " + (++d));
+        
+    }
+    
+}
